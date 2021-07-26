c59f4c7a856939c80f072f690fd9603077787d6f
charper95 <charper1995@gmail.com>
2021-07-26 00:13:48 -0700

    finished code challenges

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -75,30 +75,85 @@ console.log(fibonacci(10))
 
 // a) Create a test with expect statements for each of the variables provided.
 
+// Describe the name of the function - onlyOdd
+describe("onlyOdd", () => {
+    // Test what the function does - returns a new array of only odd numbers sorted from least to greatest
+        it("returns a new array of only odd numbers sorted from least to greatest", () =>{
+    // Expect the output of the function to be equal to the correct output - output of onlyOdd to equal an array of only odd numbers sorted from least to greatest
+            expect(onlyOdd(fullArr1)).toEqual([-9, 7, 9, 199])
+            expect(onlyOdd(fullArr2)).toEqual([-823, 7, 23])
+        })
+    })
+
 var fullArr1 = [4, 9, 0, "7", 8, true, "hey", 7, 199, -9, false, "hola"]
-// Expected output: [-9, 7, 9, 199]
 
 var fullArr2 = ["hello", 7, 23, -823, false, 78, null, "67", 6, "number"]
-// Expected output: [-823, 7, 23]
-
 
 // b) Create the function that makes the test pass.
 
+// Create a function named onlyOdd
+// That takes an array as an arguement for the input
+const onlyOdd = (array) => {
+// Use the .filter() built-in method to return an array with only odd numbers and only values that have the data type of number
+    let newArray = array.filter(value => {
+        if (value % 2 !== 0 && typeof value === "number") {
+            return value
+        }
+    })
+// Use the .sort() method to sort the array from least to greatest
+// The .sort() method must contain a compareFunction as a parameter, if it is not provided the elements of the array are converted into strings and sorted alphabetically
+// The compareFunction takes two parameters, the first element and the second element, in order to sort the numbers in ascending order you have to subtract the second element from the first element
+    return newArray.sort((a, b) => a - b)
+}
 
+console.log(onlyOdd(fullArr1))
+// Expected output: [-9, 7, 9, 199]
+
+console.log(onlyOdd(fullArr2))
+// Expected output: [-823, 7, 23]
 
 // --------------------3) Create a function that takes in an array and returns an array of the accumulating sum. An empty array should return an empty array.
 
 // a) Create a test with expect statements for each of the variables provided.
 
+// Describe the name of the function - accSum
+describe("accSum", () => {
+    // Test what the function does - returns an array of the accumulating sum of a given array
+        it("returns an array of the accumulating sum of a given array", () =>{
+    // Expect the output of the function to be equal to the correct output - output of accSum to equal an array with the accumulating sum of a given array
+            expect(accSum(numbersToAdd1)).toEqual([2, 6, 51, 60])
+            expect(accSum(numbersToAdd2)).toEqual([0, 7, -1, 11])
+            expect(accSum(numbersToAdd3)).toEqual([])
+        })
+    })
+
 var numbersToAdd1 = [2, 4, 45, 9]
-// Excpected output: [2, 6, 51, 60]
 
 var numbersToAdd2 = [0, 7, -8, 12]
-// Expected output: [0, 7, -1, 11]
 
 var numbersToAdd3 = []
-// Expected output: []
-
-
 
 // b) Create the function that makes the test pass.
+
+// Create a function named accSum
+// That takes an array as an arguement for the input
+const accSum = (array) => {
+// Assign a new array to a copy of the original array
+    let newArray = array.concat()
+// Iterate through the given array starting at index 1 because index 0 should remain the same
+    for (let i = 1; i < array.length; i++) {
+// Assign the current index of the newArray (starting from index 1) to the sum of the previous index and the current index
+        newArray[i] = newArray[i-1] + array[i]
+    }
+// After iterating through and replacing each index, return the changed array
+    return newArray
+}
+
+console.log(accSum(numbersToAdd1))
+// Excpected output: [2, 6, 51, 60]
+
+console.log(accSum(numbersToAdd2))
+// Expected output: [0, 7, -1, 11]
+
+console.log(accSum(numbersToAdd3))
+// Expected output: []
